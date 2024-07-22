64076965df8d1059d694baf69580ada53519eabe
Brian Arriaga <brian.arriaga@gmail.com>
2024-07-22 19:14:56 -0400

    Add contact form

--- base/models.py
+++ base/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+    PublishingPanel,
+    FieldRowPanel,
+    InlinePanel,
+)
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 from wagtail.fields import RichTextField
 from wagtail.models import (
@@ -9,6 +15,9 @@ from wagtail.models import (
     TranslatableMixin,
 )
 from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 
 
 @register_setting
@@ -51,3 +60,31 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer text"
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey("FormPage", on_delete=models.CASCADE, related_name="form_fields")
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel('from_address'),
+                        FieldPanel('to_address'),
+                    ]
+                ),
+                FieldPanel('subject'),
+            ],
+            "Email",
+        ),
+    ]
